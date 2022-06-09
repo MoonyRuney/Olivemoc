0276c1e8cd773aab3b4a03c7f4c3bcdd13cbfb2b
MoonyRuney <nmcguireable@gmail.com>
2022-06-09 21:10:22 +0100

    nerfed but also buffed oil

--- src/main/java/net/moonyruney/olivemod/item/ModFoodComponents.java
+++ src/main/java/net/moonyruney/olivemod/item/ModFoodComponents.java
@@ -6,7 +6,7 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
 
-    public static final FoodComponent OLIVE_OIL = new FoodComponent.Builder().hunger(5).saturationModifier(1.5f).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 2400, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1200, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 2400, 1), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 900, 0), 1.0f).build();
+    public static final FoodComponent OLIVE_OIL = new FoodComponent.Builder().hunger(5).saturationModifier(1.5f).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 12000, 4), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2400, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 18000, 2), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 1200, 0), 1.0f).build();
     public static final FoodComponent OLIVE = new FoodComponent.Builder().hunger(2).saturationModifier(0.6f).build();
 
 }
