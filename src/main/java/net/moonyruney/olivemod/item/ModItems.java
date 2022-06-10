63732b17f77c5c82db4f736a2bde37a075c8d065
MoonyRuney <nmcguireable@gmail.com>
2022-06-10 20:14:26 +0100

    added red sauce

--- src/main/java/net/moonyruney/olivemod/item/ModItems.java
+++ src/main/java/net/moonyruney/olivemod/item/ModItems.java
@@ -6,15 +6,17 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.moonyruney.olivemod.OliveMod;
-import net.moonyruney.olivemod.item.Custom.IronBrew;
+import net.moonyruney.olivemod.item.Custom.IronBrewItem;
 import net.moonyruney.olivemod.item.Custom.OliveOilItem;
+import net.moonyruney.olivemod.item.Custom.RedSauceItem;
 
 public class ModItems {
 
     public static final Item OLIVE = registerItem("olive",new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.OLIVE)));
-    public static final Item OLIVE_OIL = registerItem("oliveoil", new OliveOilItem(new FabricItemSettings(). group(ItemGroup.FOOD).food(ModFoodComponents.OLIVE_OIL)));
-    public static final Item IRON_BREW = registerItem("ironbrew",new IronBrew(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.IRON_BREW)));
+    public static final Item OLIVE_OIL = registerItem("oliveoil", new OliveOilItem(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.OLIVE_OIL)));
+    public static final Item IRON_BREW = registerItem("ironbrew",new IronBrewItem(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.IRON_BREW)));
     public static final Item BUNGER = registerItem("bunger", new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.BUNGER)));
+    public static final Item RED_SAUCE = registerItem("redsauce", new RedSauceItem(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.REDSAUCE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(OliveMod.MOD_ID, name), item);
