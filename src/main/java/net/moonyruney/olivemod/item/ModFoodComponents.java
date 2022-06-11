79625c5a04688bd002cdf94c3dde00a526d609d0
MoonyRuney <nmcguireable@gmail.com>
2022-06-11 21:02:43 +0100

    Added Trollium

--- src/main/java/net/moonyruney/olivemod/item/ModFoodComponents.java
+++ src/main/java/net/moonyruney/olivemod/item/ModFoodComponents.java
@@ -7,7 +7,7 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
 
-    public static final FoodComponent OLIVE_OIL = new FoodComponent.Builder().hunger(5).saturationModifier(2.5f).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 12000, 4), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2400, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 18000, 2), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 1200, 0), 1.0f).build();
+    public static final FoodComponent OLIVE_OIL = new FoodComponent.Builder().hunger(5).saturationModifier(2.5f).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 12000, 4), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2400, 0), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 18000, 4), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 1200, 0), 1.0f).build();
     public static final FoodComponent REDSAUCE = new FoodComponent.Builder().hunger(3).saturationModifier(1.2f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 300, 0),1.0f).build();
     public static final FoodComponent OLIVE = new FoodComponent.Builder().hunger(2).saturationModifier(0.6f).build();
     public static final FoodComponent IRON_BREW = new FoodComponent.Builder().hunger(3).saturationModifier(0.4f).statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 3600, 1),1.0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3600, 4), 1.0f).statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 3600, 2),1.0f).build();
